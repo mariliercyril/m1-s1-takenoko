b45bbe6c8b9d6c038f5cfd6092293686c6b208f6
mariliercyril <cyril.marilier@etu.unice.fr>
2018-09-20 14:17:20 +0200

    Add the global comment into the interface 'Objective'

--- src/main/java/com/raccoon/takenoko/Objective.java
+++ src/main/java/com/raccoon/takenoko/Objective.java
@@ -1,8 +1,10 @@
 package com.raccoon.takenoko;
 
+/**
+ * This interface provides methods for checking, among others, that an Objective is satisfied.
+ */
 public interface Objective {
 
     public boolean checkIfCompleted();
     public int getScore();
-
 }
