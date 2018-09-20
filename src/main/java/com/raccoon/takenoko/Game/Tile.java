a01c83aeef5d55e51f61dfedbbad19030a97ac7a
Loïc Germerie <l.germerie@free.fr>
2018-09-20 15:41:53 +0200

    Methods added in the Tile interface

--- src/main/java/com/raccoon/takenoko/Game/Tile.java
+++ src/main/java/com/raccoon/takenoko/Game/Tile.java
@@ -1,4 +1,12 @@
 package com.raccoon.takenoko.Game;
 
+import java.awt.Point;
+import java.util.List;
+
 public interface Tile {
+
+    public Point getPosition();
+    public void setPosition(Point position);
+    public List<Point> getFreeBorders();
+
 }
