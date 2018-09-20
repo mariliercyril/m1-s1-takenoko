d22ab88697a17a4f8719a2c8d7e672e4c959cd5d
mariliercyril <cyril.marilier@etu.unice.fr>
2018-09-20 15:56:18 +0200

    Implemente the Objective mechanism

--- src/main/java/com/raccoon/takenoko/Game/Objective.java
+++ src/main/java/com/raccoon/takenoko/Game/Objective.java
@@ -1,10 +1,12 @@
 package com.raccoon.takenoko.Game;
 
 /**
- * This interface provides methods for checking, among others, that an Objective is satisfied.
+ * This interface provides methods for checking, among others, that an Objective is completed.
  */
 public interface Objective {
 
-    public boolean checkIfCompleted();
+	// TODO: To rename the method "checkIfCompleted" into "isCompleted".
+    public boolean checkIfCompleted(Tile basicTile, Board hashBoard);
+
     public int getScore();
 }
