7558ce9c9fc7947db19f444c9db97bf96325ba81
Loïc Germerie <l.germerie@free.fr>
2018-09-20 15:59:34 +0200

    setFreeBorders added in Tile interface

--- src/main/java/com/raccoon/takenoko/Game/Tile.java
+++ src/main/java/com/raccoon/takenoko/Game/Tile.java
@@ -8,5 +8,6 @@ public interface Tile {
     public Point getPosition();
     public void setPosition(Point position);
     public int getFreeBorders();
+    public void setFreeBorders(int numberOfFreeNumbers);
 
 }
