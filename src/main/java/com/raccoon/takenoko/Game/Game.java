73f12f546ac41f553928636e7cb811c058dd5c20
theo <qui.theo@gmail.com>
2018-09-20 14:44:41 +0200

    Added constructors and getters for the Game class (#3)

--- src/main/java/com/raccoon/takenoko/Game/Game.java
+++ src/main/java/com/raccoon/takenoko/Game/Game.java
@@ -1,7 +1,9 @@
 package com.raccoon.takenoko.Game;
 
 import com.raccoon.takenoko.Player.Player;
+import com.raccoon.takenoko.Player.RandomBot;
 
+import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -12,4 +14,23 @@ public class Game {
     private List<Player> players;   // The Players participating the game
     private Board board;            // The game board, with all the tiles
 
+    public Game() {                 // Default constructor: 1v1 game
+        this.players = new ArrayList<>();
+        for (int i = 0; i < 2; i++) players.add(new RandomBot());
+        //todo : instanciate the board
+    }
+
+    public Game(List<Player> players) {
+        this.players = players;
+        //todo : instanciate the board
+    }
+
+    public List<Player> getPlayers() {
+        return players;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+    
 }
