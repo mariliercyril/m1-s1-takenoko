0b3dfeb7ea351c1e2ebb584688c19073e669453f
Violette Heron <heron.violette@gmail.com>
2018-09-20 15:20:52 +0200

    #1 Player computes a random position where to put down a tile

--- src/main/java/com/raccoon/takenoko/Player/Player.java
+++ src/main/java/com/raccoon/takenoko/Player/Player.java
@@ -1,13 +1,35 @@
 package com.raccoon.takenoko.Player;
 
+import com.raccoon.takenoko.Game.Game;
+
 /**
  * Class representig the player taking part in the game. To be extended by a bot to
  * actually perform a move when it's its turn to play.
  * Will provide all the attributes and methods common to all players.
  */
 public abstract class Player {
+    private int score;
 
-    // Signature to clarify, methode allowing the player to make his move
-    public abstract void play();
+    public Player() {
+        score = 0;
+    }
 
+    public int getScore() {
+        return score;
+    }
+
+    /**
+     * This method will be the one called by the game to give their turn to the players/
+     * It will be calling the methods for the players to play their turn.
+     * <p>
+     * DESIGN PATTERN : TEMPLATE METHOD
+     *
+     * @param game the game in which the player is playing
+     */
+    public void play(Game game) {
+        this.putDownTile(game);
+        score++;
+    }
+
+    protected abstract void putDownTile(Game game);
 }
--- src/main/java/com/raccoon/takenoko/Player/RandomBot.java
+++ src/main/java/com/raccoon/takenoko/Player/RandomBot.java
@@ -1,9 +1,37 @@
 package com.raccoon.takenoko.Player;
 
+import com.raccoon.takenoko.Game.Board;
+import com.raccoon.takenoko.Game.Game;
+
+import java.awt.*;
+import java.util.List;
+import java.util.Collections;
+import java.util.Objects;
+
 public class RandomBot extends Player {
 
+    public RandomBot() {
+        super();
+    }
+
+    /**
+     * Puts down a tile at a random available location on the board given by the game
+     *
+     * @param game the game in which the player is playing
+     */
     @Override
-    public void play() {
-        System.out.println("Todo :-)");
+    protected void putDownTile(Game game) {
+        Board b = game.getBoard();
+        if(Objects.isNull(b)){
+            return;
+        }
+        List availablePositions = b.getAvailablePositions();
+        Collections.shuffle(availablePositions);
+
+        Point playingPos;
+        if (availablePositions.size() > 0) {
+            playingPos = (Point) availablePositions.get(0);
+            // b.set(playingPos, new Tile());
+        }
     }
 }
