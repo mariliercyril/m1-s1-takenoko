ea230d356be12a963b0b87d0f16d844ffac0fd33
mariliercyril <cyril.marilier@etu.unice.fr>
2018-09-24 16:27:35 +0200

    Improve the code of the class 'BasicObjectiveTest'

--- src/test/java/com/raccoon/takenoko/game/BasicObjectiveTest.java
+++ src/test/java/com/raccoon/takenoko/game/BasicObjectiveTest.java
@@ -9,17 +9,16 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class BasicObjectiveTest {
 
-	private Tile firstTile = new BasicTile(new Point(0, 0));
-
-	private Board hashBoard = new HashBoard(firstTile);
-
 	@Test
 	@DisplayName("assert true when basic objective is completed")
 	public void testCheckIfCompleted() {
 
-		hashBoard.set(new Point(0, 1), new BasicTile());
-
 		Objective objective = new BasicObjective();
+
+		// Creates a first Tile and injects in the Board
+		Tile firstTile = new BasicTile(new Point(0, 0));
+		Board hashBoard = new HashBoard(firstTile);
+		hashBoard.set(new Point(0, 1), new BasicTile());
 		assertTrue(objective.isCompleted(firstTile, hashBoard));
 	}
 }
