0c24f34f7ce88c17001401301a309bec931ef98b
theo <qui.theo@gmail.com>
2018-09-24 14:13:55 +0200

    Added methods to get tiles from the deck (#12)

--- src/main/java/com/raccoon/takenoko/game/Game.java
+++ src/main/java/com/raccoon/takenoko/game/Game.java
@@ -17,6 +17,7 @@ public class Game {
     private Board board;            // The game board, with all the tiles
 
     public Game() {                 // Default constructor: 1v1 game
+
         this.players = new ArrayList<>();
         for (int i = 0; i < 2; i++) players.add(new RandomBot());
         board = new HashBoard(new BasicTile());
@@ -29,20 +30,24 @@ public class Game {
     }
 
     public Game(List<Player> players) {
+
         this.players = players;
         board = new HashBoard(new BasicTile());
         deck = initDeck();
     }
 
     public List<Player> getPlayers() {
+
         return players;
     }
 
     public Board getBoard() {
+
         return board;
     }
 
     public boolean gameOver() {     // Currently, the game is over as soon as a player reaches a score of 9
+
         for (Player p : players) {
             if (p.getScore() >= 9) return true;
         }
@@ -51,6 +56,7 @@ public class Game {
     }
 
     public void start() {           // Starts the game: while the game isn't over, each player plays
+
         int i = 0;
         while (!gameOver()) {
             players.get(i).play(this);
@@ -58,13 +64,22 @@ public class Game {
         }
     }
 
-    public Tile[] getTile() {       // For picking tiles
-        Tile[] res = new Tile[1];
-        res[0] = new BasicTile();
-        return res;
+    public Tile getTile() {         //  Takes a tile from the deck
+        return deck.remove();
+    }
+
+    public Tile[] getTiles() {       // Takes n (three) tiles from the deck
+
+        int nbrTiles = 3;           //  Number of tiles to choose from
+        Tile[] tiles = new Tile[3];
+        for (int i = 0; i < nbrTiles; i++) {
+            tiles[i] = getTile();
+        }
+        return tiles;
     }
 
     public Player getWinner() {
+
         for (Player p : players) {
             if (p.getScore() >= 9) {
                 return p;
