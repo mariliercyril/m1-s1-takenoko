2f86504066cc51dd03447cd350785bb00f050322
mariliercyril <cyril.marilier@etu.unice.fr>
2018-09-24 15:58:52 +0200

    Fix a bug on the test class 'ColorObjectiveTest'

--- src/test/java/com/raccoon/takenoko/game/ColorObjectiveTest.java
+++ src/test/java/com/raccoon/takenoko/game/ColorObjectiveTest.java
@@ -1,13 +1,13 @@
 package com.raccoon.takenoko.game;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 import java.awt.Point;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-
-public class BasicObjectiveTest {
+public class ColorObjectiveTest {
 
 	private Tile firstTile = new BasicTile(new Point(0, 0));
 
@@ -17,9 +17,10 @@ public class BasicObjectiveTest {
 	@DisplayName("assert true when basic objective is completed")
 	public void testCheckIfCompleted() {
 
-		hashBoard.set(new Point(0, 1), new BasicTile());
+		hashBoard.set(new Point(1, 0), new BasicTile());
+		hashBoard.set(new Point(2, 0), new BasicTile());
 
-		Objective objective = new BasicObjective();
+		Objective objective = new ColorObjective();
 		assertTrue(objective.isCompleted(firstTile, hashBoard));
 	}
 }
