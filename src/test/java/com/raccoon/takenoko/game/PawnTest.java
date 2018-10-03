55e991fb45071d885960cee820d395e81a56226d
theo <qui.theo@gmail.com>
2018-10-03 20:20:17 +0200

    tested the ability for the panda to move and its effect on the tile (removing one piece of bamboo if it's able to) (#33)

--- src/test/java/com/raccoon/takenoko/game/PawnTest.java
+++ src/test/java/com/raccoon/takenoko/game/PawnTest.java
@@ -18,6 +18,7 @@ public class PawnTest {
     Tile greenTile2 = new BasicTile(Color.GREEN);
     Tile pinkTile0 = new BasicTile(Color.PINK);
     Tile yellowTile0 = new BasicTile(Color.YELLOW);
+    Tile greenTile3 = new BasicTile(Color.GREEN);
 
 
     @Test
@@ -31,15 +32,17 @@ public class PawnTest {
         testBoard.set(new Point(1,2), greenTile1);
         testBoard.set(new Point(0,-1), greenTile2);
         testBoard.set(new Point(0,2), yellowTile0);
-
+        testBoard.set(new Point(1,0), greenTile3);
         testGaredner.move(testBoard, new Point(0,1));
 
 
 
         assertTrue("The tile adjacent to the tile where the gardener moved didn't grow a bamboo",greenTile1.getBambooSize() == 2);
-        testPanda.move(testBoard, new Point(1,2));
+        testPanda.move(testBoard, new Point(1,2));  // For the purpose of this test, it doesn't matter whether the panda is actually allowed to the tiles, we are only testing the effects of the panda arriving
         // The following tests also make sure that the surrounding tiles weren't affected by the panda
-        assertTrue("The tile where the panda landed had one piece of bamboo eaten",greenTile1.getBambooSize() == 1);
+        assertTrue("The tile where the panda landed didn't have one piece of bamboo eaten",greenTile1.getBambooSize() == 1);
+        testPanda.move(testBoard, new Point(1,0));
+        assertTrue("The tile where the panda landed had a piece of bamboo eaten even though there wasn't any bamboo to eat", greenTile3.getBambooSize() == 0);
         assertTrue("The tile where the gardener is moved didn't grow a bamboo",greenTile0.getBambooSize() == 2);
         assertTrue("The tile adjacent to the gardener grew a bamboo but wasn't of the same colour",pinkTile0.getBambooSize() == 1);
         assertTrue("The tile adjacent to the gardener grew a bamboo but wasn't of the same colour",yellowTile0.getBambooSize() == 1);
