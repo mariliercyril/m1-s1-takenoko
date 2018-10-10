2d880bf3335ad3a0374e1ff2842e87843c535d40
theo <qui.theo@gmail.com>
2018-10-10 19:53:17 +0200

    bambot always tries to validate panda objectives first (#52)

--- src/main/java/com/raccoon/takenoko/player/BamBot.java
+++ src/main/java/com/raccoon/takenoko/player/BamBot.java
@@ -2,12 +2,16 @@ package com.raccoon.takenoko.player;
 
 import com.raccoon.takenoko.game.Game;
 import com.raccoon.takenoko.game.Tile;
+import com.raccoon.takenoko.game.objective.Objective;
+import com.raccoon.takenoko.game.objective.panda.TwoBambooChunksPandaObjective;
 import com.raccoon.takenoko.tool.Constants;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 
 import java.awt.*;
-import java.util.HashMap;
+import com.raccoon.takenoko.game.Color;
+
+import java.util.*;
 import java.util.List;
-import java.util.Map;
 
 public abstract class BamBot extends Player {
 
@@ -91,5 +95,92 @@ public abstract class BamBot extends Player {
             }
         }
         return bestPoint;
+    }
+
+    @Override
+    protected Point whereToMovePanda(Game game, List<Point> available) {
+        //  The player choses to eat the bamboo pieces he lacks the most
+        int minValue = getStomach().get(Color.PINK);
+        Color minColor = Color.PINK;
+
+        for (Color c : Color.values()) {
+            if (getStomach().get(c) < minValue) {
+                minValue = getStomach().get(c);
+                minColor = c;
+            }
+        }
+
+        //  Looks for the first tile of the needed color
+
+        for (Point p : available) {
+            if (game.getBoard().get(p).getColor() == minColor) {
+                return p;
+            }
+        }
+
+        return available.get(0);
+    }
+
+    @Override
+    protected Action[] planActions(Game game) {
+        Action[] actionSet = new Action[]{Action.PUT_DOWN_TILE, Action.MOVE_GARDENER, Action.VALID_OBJECTIVE};  // Safety action set
+        boolean hasWantedObjective = false;
+
+        // Always wants to have one "have x bamboos of each color" objective
+        for (Objective obj : getObjectives()) {
+            if (obj instanceof TwoBambooChunksPandaObjective) {
+                hasWantedObjective = true;
+                break;
+            }
+        }
+        if (!hasWantedObjective && getObjectives().size() < Constants.MAX_AMOUNT_OF_OBJECTIVES) {   //  Rule to pick a new objective
+            actionSet[0] = Action.DRAW_OBJECTIVE;
+        }
+
+        if (isMovingPandaUseful(game) && needToEatBamboo()) { // Rule to move the panda
+            actionSet[1] = Action.MOVE_PANDA;
+        }
+
+        return actionSet;
+    }
+
+    private boolean isMovingPandaUseful(Game game) {    //  Tells if there is any bamboo to be eaten
+        for (Point p : game.getBoard().getAccessiblePositions(game.getPanda().getPosition())) {
+            if (game.getBoard().get(p).getBambooSize() > 0) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private boolean needToEatBamboo() { //  The player wants to always have three bamboo of each color
+        for (Color c : Color.values()) {
+            if (getStomach().get(c) < 3) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    @Override
+    protected Objective chooseObjectiveToValidate() {   // The player validates the bamboo objectives first
+        // For now, he doesn't try to validate the highest-scoring objective
+        List<Objective> completedObjectives = new ArrayList<>();
+        for (Objective obj : getObjectives()) {
+            if (obj.isCompleted()) {
+                completedObjectives.add(obj);
+            }
+         }
+
+         if (completedObjectives.size() != 0) {
+             for (Objective completed : completedObjectives) {
+                 if (completed instanceof TwoBambooChunksPandaObjective) {
+                     return completed;
+                 }
+             }
+             return completedObjectives.get(0);
+         }
+
+        return null;
     }
 }
