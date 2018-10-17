d874d922f713e55e803470982c564da9ae64e1dd
mariliercyril <cyril.marilier@etu.unice.fr>
2018-10-17 11:56:26 +0200

    Update the Javadoc on 'UnitVector' and 'PandaObjective' (#60)

--- src/main/java/com/raccoon/takenoko/game/objective/panda/PandaObjective.java
+++ src/main/java/com/raccoon/takenoko/game/objective/panda/PandaObjective.java
@@ -7,15 +7,13 @@ import com.raccoon.takenoko.game.objective.Objective;
 import com.raccoon.takenoko.player.Player;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map.Entry;
-import java.util.Set;
 
 /**
- * The {@code TwoBambooChunksPandaObjective} class implements the <i>panda</i> {@link Objective}
- * which consists in "<b>having eaten (at least) two bamboo chunks of which the color is an expected color</b>".
+ * The {@code PandaObjective} class implements the <i>panda</i> {@link Objective}
+ * which consists in "<b>having eaten (at least) two bamboo chunks of which the color is an expected color</b>"
+ * or in "<b>having eaten three bamboo chunks of which the color is different from other</b>".
  * <p>
- * The score base is equal to 3; consequently, the scores are:
+ * The score base, for the first Objective type, is equal to 3; consequently, the scores are:
  * <ul>
  * <li>3 for GREEN bamboo chunks</li>
  * <li>4 for YELLOW bamboo chunks</li>
@@ -47,18 +45,25 @@ public class PandaObjective extends Objective {
 
 		// Is completed if the size of the bamboo which is on the final tile is at least equal to 2
 		// and the color is the expected color (color of Objective)
+		// Or if the size of the bamboo which is on the final tile is at least equal to 1
+		// and each tile in question has a color different from other
 		if (stomach.entrySet().stream().anyMatch(b -> ((b.getKey()).equals(color) && (b.getValue()) >= 2))) {
 			isCompleted = true;
 		} else {
-			stomach.entrySet().stream().forEach(b1 ->
-				stomach.entrySet().stream().forEach(b2 ->
-					stomach.entrySet().stream().forEach(b3 -> {
-						if (!(b2.getKey()).equals(b1.getKey()) && !(b3.getKey()).equals(b1.getKey()) && !(b3.getKey()).equals(b2.getKey())) {
-							isCompleted = true;
+			stomach.entrySet().stream().forEach(b1 -> {
+				if ((b1.getValue()) >= 1) {
+					stomach.entrySet().stream().forEach(b2 -> {
+						if ((b2.getValue()) >= 1) {
+							stomach.entrySet().stream().filter(b3 -> ((b3.getValue()) >= 1)).forEach(b3 -> {
+								if (!(b2.getKey()).equals(b1.getKey()) && !(b3.getKey()).equals(b1.getKey()) && !(b3.getKey()).equals(b2.getKey())) {
+									isCompleted = true;
+									score = SCORE_BASE + SCORE_BASE;
+								}
+							});
 						}
-					})
-				)
-			);
+					});
+				}
+			});
 		}
 	}
 
