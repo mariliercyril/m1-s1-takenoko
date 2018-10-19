6c349955bd1f0530488f3724591c883a29884e9e
mariliercyril <cyril.marilier@etu.unice.fr>
2018-10-19 22:31:51 +0200

    Refactor the 'PandaObjective' classes

--- src/main/java/com/raccoon/takenoko/game/objective/PandaObjective.java
+++ src/main/java/com/raccoon/takenoko/game/objective/PandaObjective.java
@@ -6,19 +6,18 @@ import com.raccoon.takenoko.game.objective.Objective;
 
 import com.raccoon.takenoko.player.Player;
 
-import java.util.HashMap;
 import java.util.Map;
 
 /**
  * The {@code PandaObjective} class implements the <i>panda</i> {@link Objective}
- * which consists in "<b>having eaten (at least) two bamboo chunks of which the color is an expected color</b>"
- * or in "<b>having eaten three bamboo chunks of which the color is different from other</b>".
+ * which consists in "<b>having eaten at least two bamboo chunks of which the color is an expected color</b>"
+ * or in "<b>having eaten at least one bamboo chunk of each of the color</b> (GREEN, YELLOW, PINK)".
  * <p>
  * The score base, for the first Objective type, is equal to 3; consequently, the scores are:
  * <ul>
- * <li>3 for GREEN bamboo chunks</li>
- * <li>4 for YELLOW bamboo chunks</li>
- * <li>5 for PINK bamboo chunks</li>
+ * <li>3 for (at least) two GREEN bamboo chunks</li>
+ * <li>4 for (at least) two YELLOW bamboo chunks</li>
+ * <li>5 for (at least) two PINK bamboo chunks</li>
  * </ul>
  */
 public class PandaObjective extends Objective {
@@ -26,7 +25,8 @@ public class PandaObjective extends Objective {
 	private static final int SCORE_BASE = 3;
 
 	/**
-	 * Constructs a {@code TwoBambooChunksPandaObjective} with a specified color.
+	 * Constructs a {@code PandaObjective} of the first type
+	 * (i.e. a {@code PandaObjective} <i>with a specified color</i>).
 	 * 
 	 * @param color
 	 *  the color of the bamboo chunks which should have been eaten
@@ -35,41 +35,37 @@ public class PandaObjective extends Objective {
 
 		super();
 		this.color = color;
-		setScore(color);
+		setScore(this.color);
+	}
+
+	/**
+	 * Constructs a {@code PandaObjective} of the second type.
+	 */
+	public PandaObjective() {
+
+		super();
+		score = SCORE_BASE + SCORE_BASE;
 	}
 
 	@Override
 	public void checkIfCompleted(Player player) {
 
-		// Gets the size of the bamboo which is on the final tile
+		// Gets the player's stomach to dissect it
 		Map<Color, Integer> stomach = player.getStomach();
 
-		// Is completed if the size of the bamboo which is on the final tile is at least equal to 2
-		// and the color is the expected color (color of Objective)
-		// Or if the size of the bamboo which is on the final tile is at least equal to 1
-		// and each tile in question has a color different from other
-		if (stomach.entrySet().stream().anyMatch(b -> ((b.getKey()).equals(color) && (b.getValue()) >= 2))) {
+		// Is completed if the stomach in question contains at least 2 bamboo chunks
+		// with the expected color (color of Objective)
+		// or if it contains at least 1 bamboo chunk per color
+		if (stomach.get(color) > 1) {
 			isCompleted = true;
-		} else {
-			stomach.entrySet().stream().forEach(b1 -> {
-				if ((b1.getValue()) >= 1) {
-					stomach.entrySet().stream().forEach(b2 -> {
-						if ((b2.getValue()) >= 1) {
-							stomach.entrySet().stream().filter(b3 -> ((b3.getValue()) >= 1)).forEach(b3 -> {
-								if (!(b2.getKey()).equals(b1.getKey()) && !(b3.getKey()).equals(b1.getKey()) && !(b3.getKey()).equals(b2.getKey())) {
-									isCompleted = true;
-									score = SCORE_BASE + SCORE_BASE;
-								}
-							});
-						}
-					});
-				}
-			});
+		} else if (stomach.values().stream().allMatch(n -> n > 0)) {
+			isCompleted = true;
 		}
 	}
 
 	/**
-	 * Sets the score according to the color.
+	 * Sets the score according to the color
+	 * (if the {@code PandaObjective} of the first type is completed).
 	 * 
 	 * @param color
 	 *  the color which is associated with the objective
