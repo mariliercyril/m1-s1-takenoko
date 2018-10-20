3782a1d3cfcbc69efd0a57b1e10d58ab916274f9
Loïc Germerie <l.germerie@free.fr>
2018-10-20 16:00:23 +0200

    Correction of an irrigation test #59

--- src/test/java/com/raccoon/takenoko/game/HashBoardTest.java
+++ src/test/java/com/raccoon/takenoko/game/HashBoardTest.java
@@ -120,8 +120,7 @@ public class HashBoardTest {
 
     @Test
     public void irrigateTest() {
-        // Testing if a tile can be irrigated toward the pond ??
-        //assertTrue("Irrigation can't be put between to correct tiles", b.irrigate(t2.getPosition(), UnitVector.K));
+        assertTrue("Irrigation can't be put between to correct tiles", b.irrigate(t1.getPosition(), UnitVector.J));
         assertFalse("Irrigation put on a tile with no other tile adjacent", b.irrigate(t3.getPosition(), UnitVector.K.opposite()));
 
     }
