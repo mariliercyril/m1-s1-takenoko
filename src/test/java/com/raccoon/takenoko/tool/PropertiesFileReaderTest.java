b9974be3c30d5d6a90e33d2d09526444915cedfd
mariliercyril <cyril.marilier@etu.unice.fr>
2018-10-24 12:42:00 +0200

    Implement unit tests of the 'PropertiesFileReader'

---
+++ src/test/java/com/raccoon/takenoko/tool/PropertiesFileReaderTest.java
@@ -0,0 +1,43 @@
+package com.raccoon.takenoko.tool;
+
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+/**
+ * This class allows to complete test of the class {@link PropertiesFileReader}.
+ */
+public class PropertiesFileReaderTest {
+
+	private static PropertiesFileReader propertiesFileReader;
+
+	@BeforeAll
+	public static void createsMockPlayer() {
+
+		propertiesFileReader = PropertiesFileReader.getInstance();
+	}
+
+	/*
+	 * When the CheckIfCompleted method is used with a tile and a board (as paramters)
+	 */
+	@Test
+	@DisplayName("assert that it throws CloneNotSupportedException when we try to clone the PropertiesFileReader")
+	public void testClone() {
+
+        assertThrows(CloneNotSupportedException.class, () -> propertiesFileReader.clone());
+	}
+
+	/*
+	 * When the CheckIfCompleted method is used with a tile and a board (as paramters)
+	 */
+	@Test
+	@DisplayName("assert that it throws NullPointerException"
+			+ "when the file of which the name is as a parameter is not found by the PropertiesFileReader")
+	public void testReadPropertiesFile() {
+
+        assertThrows(NullPointerException.class, () -> propertiesFileReader.getStringProperty("fileName", "key", "defaultValue"));
+	}
+
+}
