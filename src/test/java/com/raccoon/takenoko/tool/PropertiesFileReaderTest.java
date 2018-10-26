cc7d86fce90cd56d0677f18d347bc503cdb6c81f
mariliercyril <cyril.marilier@etu.unice.fr>
2018-10-26 20:33:04 +0200

    Complete the 'PropertiesFileReader'

--- src/test/java/com/raccoon/takenoko/tool/PropertiesFileReaderTest.java
+++ src/test/java/com/raccoon/takenoko/tool/PropertiesFileReaderTest.java
@@ -1,7 +1,6 @@
 package com.raccoon.takenoko.tool;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.apache.log4j.Appender;
 import org.apache.log4j.Logger;
