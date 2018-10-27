81eacae815ba0b3d4084a6b4c38baff93cf4ef0a
mariliercyril <cyril.marilier@etu.unice.fr>
2018-10-27 17:48:31 +0200

    Improve test logging to console

--- src/main/java/com/raccoon/takenoko/tool/PropertiesFileReader.java
+++ src/main/java/com/raccoon/takenoko/tool/PropertiesFileReader.java
@@ -67,7 +67,9 @@ public final class PropertiesFileReader {
 			Properties properties = this.getProperties(String.format(PROPERTIES_FILE_PATH_FORMAT, propertiesFileName));
 			property = properties.getProperty(String.format(KEY_NAME_FORMAT, propertiesFileName, key), defaultValue);
 		} catch (NullPointerException | IOException e) {
-			LOGGER.error(PropertiesFileReader.class.getCanonicalName() + Separator.POINT.getSign() + "getProperties()" + " ", e);
+			LOGGER.error(PropertiesFileReader.class.getCanonicalName()
+					+ Separator.POINT.getSign() + "getProperties()"
+					+ Separator.COLON.getSign(), e);
 		}
 
 		return property;
