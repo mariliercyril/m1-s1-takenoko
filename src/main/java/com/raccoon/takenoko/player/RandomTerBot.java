c9a013102c1b5bc2c8c8be59898f08b28a77ada8
theo <qui.theo@gmail.com>
2018-12-04 16:16:56 +0100

    random ter bot (#100)

---
+++ src/main/java/com/raccoon/takenoko/player/RandomTerBot.java
@@ -0,0 +1,38 @@
+package com.raccoon.takenoko.player;
+
+import com.raccoon.takenoko.game.Game;
+import com.raccoon.takenoko.game.objective.ObjectivePool;
+import com.raccoon.takenoko.game.objective.ObjectiveType;
+import com.raccoon.takenoko.tool.Constants;
+
+import java.awt.*;
+import java.util.*;
+import java.util.List;
+
+public class RandomTerBot extends RandomBot {
+
+    @Override
+    protected Action[] planActions(Game game) {
+        if (this.getObjectives().size() < Constants.MAX_AMOUNT_OF_OBJECTIVES) {
+            return new Action[]{Action.MOVE_PANDA, Action.DRAW_OBJECTIVE, Action.VALID_OBJECTIVE};
+        }
+        return new Action[]{Action.MOVE_PANDA, Action.VALID_OBJECTIVE};
+    }
+
+    @Override
+    protected Point whereToMovePanda(Game game, List<Point> available) {
+        Point safetyNet = available.get(0);
+        available.removeIf(p -> game.getBoard().get(p).getBambooSize() == 0);
+        if (available.isEmpty()) {
+            return safetyNet;
+        } else {
+            Collections.shuffle(available);
+            return available.get(0);
+        }
+    }
+
+    @Override
+    protected ObjectiveType whatTypeToDraw(ObjectivePool pool) {
+       return ObjectiveType.PANDA;
+    }
+}
